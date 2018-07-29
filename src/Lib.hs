8f3ef9940e02bf057ef3019edf80fc6a4227e6df
Chris Penner <christopher.penner@gmail.com>
2018-07-29 13:49:38 -0600

    Working zippers!

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,19 +9,67 @@
 module Lib where
 
 import Control.Applicative
+import Control.Comonad
 import Control.Comonad.Cofree
 import Control.Lens hiding ((:<))
+import Data.Bifunctor.Fix
 import Data.Functor.Classes
 import Data.Functor.Compose
+import Data.Functor.Sum
 
-newtype Fix1 f a =
-  Fix1 (f (Fix1 f) a)
-
-data Parent f a
-  = Parent (Cofree (Compose f (Either ())) a)
-  | NoParent
+data Zip f a =
+  Zip ([Cofree (Compose f Maybe) a])
+      (Cofree f a)
   deriving (Functor, Show)
 
+-- deriving instance (Show1 f, Show a) => Show (Zip f a)
+zipCofree :: Cofree f a -> Zip f a
+zipCofree fa = Zip [] fa
+
+extract :: Zip f a -> a
+extract (Zip _ (a :< _)) = a
+
+hoistMaybe :: Functor f => f a -> Compose f Maybe a
+hoistMaybe = Compose . fmap pure
+
+up ::
+     forall f a. Functor f
+  => Zip f a
+  -> Zip f a
+up z@(Zip [] _) = z
+up (Zip ((p :< pcfr):ps) cfr) = Zip ps newFocus
+  where
+    newFocus :: Cofree f a
+    newFocus = p :< blah pcfr
+    blah :: Compose f Maybe (Cofree (Compose f Maybe) a) -> f (Cofree f a)
+    blah (Compose fm) = fmap blah2 fm
+    blah2 :: Maybe (Cofree (Compose f Maybe) a) -> Cofree f a
+    blah2 Nothing = cfr
+    blah2 (Just (a :< as)) = a :< blah as
+
+down ::
+     forall f a. Alternative f
+  => (forall b. Traversal' (f b) b)
+  -> Zip f a
+  -> Zip f a
+down t z@(Zip oldParents cfr@(_ :< fa)) =
+  case (fa ^? t) of
+    Nothing -> z
+    Just next -> Zip (newParent : oldParents) next
+  where
+    newParent :: Cofree (Compose f Maybe) a
+    newParent = setP $ hoistCofree hoistMaybe cfr
+    setP :: Cofree (Compose f Maybe) a -> Cofree (Compose f Maybe) a
+    setP (a :< Compose fa') = a :< Compose (set t Nothing fa')
+
+exampleM :: Zip Maybe Char
+exampleM = zipCofree ('a' :< Just ('b' :< Just ('c' :< Nothing)))
+
+exampleL :: Zip [] Char
+exampleL = zipCofree ('a' :< ['b' :< ['d' :< []], 'c' :< []])
+
+exampleCofree :: Cofree [] Char
+exampleCofree = ('a' :< ['b' :< ['d' :< []], 'c' :< []])
 -- instance Functor f => Functor (Parent f) where
 --   fmap :: forall a b. (a -> b) -> Parent f a -> Parent f b
 --   fmap _ NoParent = NoParent
@@ -34,19 +82,10 @@ data Parent f a
 --       mapInner = fmap (bimap f (fmap (fmap f)))
 -- type Parent f a = Maybe (f (Either a (Cofree f)))
 -- type Parent f a = Fix1 (Compose (Compose Maybe (Cofree f))) a
-data Zip f a =
-  Zip (Parent f a)
-      (Cofree f a)
-  deriving (Functor)
-
-deriving instance (Show1 f, Show a) => Show (Zip f a)
-
-zipCofree :: Cofree f a -> Zip f a
-zipCofree fa = Zip NoParent fa
-
-extract :: Zip f a -> a
-extract (Zip _ (a :< _)) = a
-
+-- 
+-- 
+-- 
+-- 
 -- down ::
 --      forall f a. Alternative f
 --   => (forall b. Traversal' (f b) b)
@@ -60,52 +99,10 @@ extract (Zip _ (a :< _)) = a
 --     newParent :: Parent f a
 --     newParent =
 --       case oldParent of
---         NoParent -> Parent (set t (a :< empty) fa)
---         Parent oldParent' -> Parent $ set t (a :< oldParent') fa
-hoistEither :: Functor f => f a -> Compose f (Either ()) a
-hoistEither = Compose . fmap Right
-
-down ::
-     forall f a. Alternative f
-  => (forall b. Traversal' (f b) b)
-  -> Zip f a
-  -> Zip f a
-down t z@(Zip oldParent (a :< fa)) =
-  case (fa ^? t) of
-    Nothing -> z
-    Just next -> Zip newParent next
-  where
-    newParent :: Parent f a
-    newParent =
-      case oldParent of
-        NoParent ->
-          Parent $
-          hoistCofree (Compose . set t (Left ()) . getCompose) $
-          hoistCofree hoistEither (a :< fa)
-        Parent oldParent' ->
-          let newLevel = hoistCofree hoistEither (a :< fa)
-           in Parent $ embed newLevel oldParent'
-
-embed ::
-     Functor f
-  => Cofree (Compose f (Either ())) a
-  -> Cofree (Compose f (Either ())) a
-  -> Cofree (Compose f (Either ())) a
-embed newLevel (a :< Compose fa) =
-  (a :< Compose (fmap (fmap (embed newLevel)) (switchLefts newLevel <$> fa)))
-  where
-    switchLefts :: forall b. b -> Either () b -> Either () b
-    switchLefts a' (Left ()) = Right a'
-    switchLefts _ r = r
-
--- up :: Zip Maybe a -> Zip Maybe a
--- up z@(Zip Nothing _ _) = z
--- up (Zip (Just (pa :< pfa)) a fa) = Zip newParent pa (pure (a :< fa))
---   where
---     newParent :: Maybe (Cofree (Maybe (Either ())) a)
---     newParent = either (const pfa) id <$> pfa
-exampleM :: Zip Maybe Char
-exampleM = zipCofree ('a' :< Just ('b' :< Just ('c' :< Nothing)))
-
-exampleL :: Zip [] Char
-exampleL = zipCofree ('a' :< ['b' :< ['d' :< []], 'c' :< []])
+--         NoParent ->
+--           Parent $
+--           hoistCofree (Compose . set t Nothing . getCompose) $
+--           hoistCofree hoistMaybe (a :< fa)
+--         Parent oldParent' ->
+--           let newLevel = hoistCofree hoistMaybe (a :< fa)
+--            in Parent $ embed newLevel oldParent'
